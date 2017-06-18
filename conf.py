2dabd9b444943834a84f5a213e3fabba76bbc1b5
Crows Of War <ofwarcrows@gmail.com>
2017-06-17 19:29:05 -0500

    Try new theme?

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-
+import sphinx_bootstrap_theme
 
 # -- General configuration ------------------------------------------------
 
@@ -83,7 +83,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -152,6 +153,3 @@ texinfo_documents = [
      author, 'AvatarMod-Docs', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
