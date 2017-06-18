2b99f8f88c7dbe0dbecffc33f057a5b42c370aad
Crows Of War <ofwarcrows@gmail.com>
2017-06-17 19:33:17 -0500

    Try default theme

--- conf.py
+++ conf.py
@@ -20,7 +20,6 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-import sphinx_bootstrap_theme
 
 # -- General configuration ------------------------------------------------
 
@@ -83,8 +82,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'bootstrap'
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
